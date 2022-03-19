6fa8b531d0815fb697f6d395f51b16fbaa260fbf
eviemaygriffith <80247859+eviemaygriffith@users.noreply.github.com>
2022-03-19 14:39:37 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,21 @@
+
+    <footer class="container-fluid text-center">
+        <div class="container">
+            <div class="row">
+<div class="col-md-6">
+  <a class="navbar-brand" href="#"><span class="josh">JOSH</span><span class="orange"> WHITKIN</span></a>
+
+</div> <!-- column 1 -->
+<div class="col-md-6">
+    <p>Terms and Conditions</p>
+    
+    </div> <!-- column 2 -->
+
+            </div><!--row -->
+        </div> <!-- container -->
+
+
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
